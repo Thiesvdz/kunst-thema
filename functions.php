72f164a4b98a3712c1332c992c3f12b57270641a
Pawel <smrpawel@gmail.com>
2022-01-14 12:19:32 +0100

    Stylesheets for elke page + scss watch script

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 function kunst_adding_scripts() {
 
     wp_enqueue_style('homestyle', get_template_directory_uri() . '/css/homestyle.css', array(), time());
+    wp_enqueue_style('home', get_template_directory_uri() . '/css/home.css', array(), time());
+    wp_enqueue_style('about-us', get_template_directory_uri() . '/css/about-us.css', array(), time());
+    wp_enqueue_style('events', get_template_directory_uri() . '/css/events.css', array(), time());
+    wp_enqueue_style('movies', get_template_directory_uri() . '/css/movies.css', array(), time());
     wp_enqueue_script('kunst-script', get_template_directory_uri() . '/js/script.js');
 }
 add_action('wp_enqueue_scripts','kunst_adding_scripts');
