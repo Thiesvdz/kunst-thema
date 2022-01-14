6182f339663bb7b0a54befa25089e2d05d3a39e3
Thies <Thiesvanderzon@gmail.com>
2022-01-14 13:08:14 +0100

    event page begin

--- page-32.php
+++ page-32.php
@@ -4,8 +4,13 @@
 get_header();
 ?>
 
-<div class="content">
-    <p>HALO</p>
+<div class="event-content">
+
+    <?php if (has_post_thumbnail()):?>
+        <?php the_post_thumbnail('medium');?>
+    <?php endif ?>
+
+    <div class="desc"><?php the_content() ?></div>
 </div>
 
 <?php
