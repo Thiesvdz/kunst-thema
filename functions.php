602c4fd2b392b3eb3614e444fc7370857a27aed0
pawelkellner <karkuwa@Pawel-MacBook.local>
2022-01-27 12:04:01 +0100

    WooCommerce file + template aanpassen

--- functions.php
+++ functions.php
@@ -18,6 +18,19 @@ add_action( 'init', 'kunst_init_thema' );
 function kunst_setup_thema(){
     add_theme_support('post-thumbnails');
     set_post_thumbnail_size( 100, 100 );
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 300,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+    ) );
 }
 
 
