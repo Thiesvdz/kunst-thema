938054d52448b54b25ae046266c692d3eb860cb9
Thies <Thiesvanderzon@gmail.com>
2022-02-01 10:43:19 +0100

    footer update en styling

--- footer.php
+++ footer.php
@@ -3,27 +3,27 @@
     <div class="footer-heel">
         <div class="location">
             <h2 class="Location-tekst">Location</h2>
-            <p >
-                <a href="https://www.google.com/maps/place/Eye+Filmmuseum/@52.384348,4.8990873,17z/data=!3m1!4b1!4m5!3m4!1s0x47c609b44d95e1d9:0xb322c46184413eb9!8m2!3d52.384348!4d4.901276" target="_blank" class="tekst">IJpromenade 1,<br>1031 KT Amsterdam</a>
+            <p class="footer-p">
+                <a href="https://www.google.com/maps/place/Eye+Filmmuseum/@52.384348,4.8990873,17z/data=!3m1!4b1!4m5!3m4!1s0x47c609b44d95e1d9:0xb322c46184413eb9!8m2!3d52.384348!4d4.901276" target="_blank" class="tekst">IJpromenade 1 <br>1031 KT Amsterdam</a>
             </p>
         </div>
 
         <div class="Socials">
             <h2 class="bogo">Socials</h2>
             <div class="logos">
-                <img src="<?php echo get_template_directory_uri()?>/Footer-img/instagram-brands.svg"  class="footer-socials">
-                <img src="<?php echo get_template_directory_uri()?>/Footer-img/facebook-square-brands.svg"  class="footer-socials">
-                <img src="<?php echo get_template_directory_uri()?>/Footer-img/youtube-brands (1).svg"  class="footer-socials">
-                <img src="<?php echo get_template_directory_uri()?>/Footer-img/twitter-brands.svg"  class="footer-socials">
-                <img src="<?php echo get_template_directory_uri()?>/Footer-img/linkedin-brands.svg"  class="footer-socials">
+                <a href="https://www.instagram.com/eye_film/" target="_blank"><img src="<?php echo get_template_directory_uri()?>/Footer-img/instagram-brands.svg"  class="footer-socials"></a>
+                <a href="https://www.facebook.com/eyefilm/" target="_blank"><img src="<?php echo get_template_directory_uri()?>/Footer-img/facebook-square-brands.svg"  class="footer-socials"></a>
+                <a href="https://www.youtube.com/user/eyefilmNL" target="_blank"><img src="<?php echo get_template_directory_uri()?>/Footer-img/youtube-brands (1).svg"  class="footer-socials"></a>
+                <a href="https://twitter.com/EYE_film?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><img src="<?php echo get_template_directory_uri()?>/Footer-img/twitter-brands.svg"  class="footer-socials"></a>
+                <a href="https://www.linkedin.com/company/eyefilm/?originalSubdomain=nl" target="_blank"><img src="<?php echo get_template_directory_uri()?>/Footer-img/linkedin-brands.svg"  class="footer-socials"></a>
             </div>
         </div>
 
         <div class="contact">
             <h2>Contact us</h2>
-            <p>
+            <p class="footer-p">
                 <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=Info@eyefilm.nl" target="_blank" class="tekst"> Info@eyefilm.nl </a>
-                <p>020-5891400</p>
+                <p class="footer-p">020-5891400</p>
             </p>
         </div>
     </div>
--- woocommerce.php
+++ woocommerce.php
@@ -3,7 +3,7 @@
     <?php
         get_header();
     ?>
-    <section class="product">
+    <section class="product ">
         <?php woocommerce_content(); ?>
     </section>
     
